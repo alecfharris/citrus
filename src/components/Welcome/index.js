446edf31151d131676e6ea9f52374078222c10c5
Paul Coroneos <paul@pcoroneos.com>
2019-01-26 12:52:42 -0600

    align with Alec on routes for backend

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import Recipe from "../Recipe";
 import FridgeList from "../FridgeList";
 import { Switch, Route } from "react-router-dom";
+import Browse from "../Browse";
 
 const Welcome = () => {
   <Switch>
     <Route exact path="/fridgelist" component={FridgeList} />
     <Route path="/recipe" component={Recipe} />
+    <Route path="/browse" component={Browse} />
   </Switch>;
 };
 
