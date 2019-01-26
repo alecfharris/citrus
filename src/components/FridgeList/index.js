34c896bc8650810bfc5467dcbdf1c94585b9f79a
Renee Wysocki <reneew311@gmail.com>
2019-01-26 14:03:43 -0600

    some component creation

--- src/components/FridgeList/index.js
+++ src/components/FridgeList/index.js
@@ -1,11 +1,29 @@
 import React from "react";
-import StyledList from "./style.js";
 import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import FridgeItemInfo from "../FridgeItemInfo";
 
-Fridgelist.propTypes = {
-  text: PropTypes.string
+const styles = theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    background: "#FFF"
+  }
+});
+
+function FridgeList(props) {
+  const { classes } = props;
+  return (
+    <List className={classes.root}>
+      <FridgeItemInfo title="Apples" description="2 Remaining" />
+      <FridgeItemInfo title="Butter" description="1/4 Pound Remaining" />
+    </List>
+  );
+}
+
+FridgeList.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-const Fridgelist = ({ text }) => <StyledList>{text}</StyledList>;
-
-export default Fridgelist;
+export default withStyles(styles)(FridgeList);
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,21 +5,19 @@ import Logo from "../Logo";
 import Button from "../Button";
 import HeaderText from "../HeaderText";
 import PasswordField from "../PasswordField";
-import RecipeCard from "../RecipeCard";
+import FridgeList from "../FridgeList";
+import HeadNav from "../HeadNav/index.js";
 
 const Home = () => (
   <StyledHome>
+    <HeadNav pageTitle="The Pantry Project" />
     <HeaderText text="CREATE AN ACCOUNT" />
     <Logo />
     <InputField placeholder="Username" />
     <PasswordField placeholder="Password" />
     <Button text="LOG IN" />
     <Button text="CREATE ACCOUNT" />
-    <RecipeCard
-      imgsrc="https://picsum.photos/300/200/?random"
-      title="This is a recipe title"
-      description="This is a recipe description."
-    />
+    <FridgeList />
   </StyledHome>
 );
 
