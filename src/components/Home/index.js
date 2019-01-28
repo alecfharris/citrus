4fffbf6dda3cbc942439ec4b8a279f9dbea8909f
Renee Wysocki <reneew311@gmail.com>
2019-01-28 17:55:26 -0600

    eslint

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,14 +6,12 @@ import Button from "../Button";
 import HeaderText from "../HeaderText";
 import PasswordField from "../PasswordField";
 import FridgeList from "../FridgeList";
-import HeadNav from "../HeadNav/index.js";
 
 const Home = () => (
   <StyledHome>
-    <HeadNav pageTitle="The Pantry Project" />
     <HeaderText text="CREATE AN ACCOUNT" />
     <Logo />
-    <InputField placeholder="Username" />
+    <InputField />
     <PasswordField placeholder="Password" />
     <Button text="LOG IN" />
     <Button text="CREATE ACCOUNT" />
