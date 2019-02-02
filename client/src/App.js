d3b38b0cdb26427a6516051755754797635048b4
Paul Coroneos <paul@pcoroneos.com>
2019-02-02 11:26:04 -0600

    implement initial routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './components/Login';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
-    return <Login />;
+    return <Home />;
   }
 }
 
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // import Creation from '../Creation';
 import Login from '../Login';
+import FridgeInfo from '../FridgeInfo';
+import FridgeList from '../FridgeList';
+import Welcome from '../Welcome';
+// import RecipeBrowse from '../RecipeBrowse';
 
 const Home = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
-    {/* <Route path="/create" component={Creation} /> */}
+    <Route exact path="/fridgeinfo" component={FridgeInfo} />
+    <Route exact path="/fridgelist" component={FridgeList} />
+    <Route exact path="/welcome" component={Welcome} />
+    {/* <Route exact path="/recipes" component={RecipeBrowse} /> */}
   </Switch>
 );
 
