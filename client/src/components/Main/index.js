c6272337406037bbdea40159508199e788f6d1fc
Paul Coroneos <paul@pcoroneos.com>
2019-02-06 20:29:44 -0600

    catching up

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -5,6 +5,7 @@ import Browse from '../Browse';
 import Welcome from '../Welcome';
 import Login from '../Login';
 import Home from '../Home';
+import EnterRecipe from '../EnterRecipe';
 
 const Main = () => (
   <BrowserRouter>
@@ -13,6 +14,7 @@ const Main = () => (
       <Route exact path="/fridgelist" component={FridgeList} />
       <Route exact path="/browse" component={Browse} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/enterrecipe" component={EnterRecipe} />
       <Route path="/home" component={Home} />
     </Switch>
   </BrowserRouter>
