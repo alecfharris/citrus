fe5c8b80d84cbf36ce57ac62c3eca6a7765a4e72
Paul Coroneos <paul@pcoroneos.com>
2019-02-07 22:13:35 -0600

    restore component

--- client/src/components/EnterRecipe/index.js
+++ client/src/components/EnterRecipe/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Component } from 'react';
-// import API from '../../utils/API';
-// import IngredientInput from '../IngredientInput';
+import API from '../../utils/API';
+import IngredientInput from '../IngredientInput';
 
-// const styles = () => ({
-//   root: {
-//     width: '100%',
-//     maxWidth: 360,
-//     background: '#FFF',
-//   },
-// });
+// Database Queries go here
+// const saveRecipe = (props) => {
+//     API.saveRecipe({
+//         title: props.title,
+//         ingredients: props.ingredients,
+//         instructions: props.instructions,
+//         // TODO change accountId to real value once possible
+//         accountId: 't0d0r3m0v3l8rt3st64'
+//       })
+// }
+
+// Event.target
 
 class EnterRecipe extends Component {
   render() {
     return (
-      <div>
-        Hello!
-        <div>Hello2</div>
+      <div
+        style={{
+          margin: '8px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <IngredientInput />
       </div>
+      // Form Views go here (from a separate file)
     );
   }
 }
