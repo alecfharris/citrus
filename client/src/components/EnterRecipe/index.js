3641c0ae746b111641953fb8f4e5d9fccc12798a
Renee Wysocki <reneew311@gmail.com>
2019-02-09 16:21:01 -0600

    enter recipe

--- client/src/components/EnterRecipe/index.js
+++ client/src/components/EnterRecipe/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import IngredientInput from '../IngredientInput';
+import NavBar from '../NavBar';
+import RecipeInput from '../RecipeInput';
+import StyledRecipeEntry from './style';
 
 class EnterRecipe extends Component {
   render() {
