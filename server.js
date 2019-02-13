a9057942c7a6e9e82c5d4e2ec10e512fa63d81e4
Paul Coroneos <paul@pcoroneos.com>
2019-02-12 23:26:15 -0600

    test

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+console.log('static path is ', path.join(__dirname, 'client/build'));
+
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/client/build/index.html`));
