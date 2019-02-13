39202476106b8331f739c8f0b00e33340d2e1929
Paul Coroneos <paul@pcoroneos.com>
2019-02-12 22:56:29 -0600

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import Main from './components/Main';
+import { Route, Switch } from 'react-router-dom';
+import Fridge from './components/Fridge';
+import Browse from './components/Browse';
+import Welcome from './components/Welcome';
+import Login from './components/Login';
+import Home from './components/Home';
+import EnterRecipe from './components/EnterRecipe';
+import RecipePage from './components/RecipePage';
+import SearchByIngredient from './components/SearchByIngredient';
 
 class App extends Component {
   render() {
-    return <Main />;
+    const App = () => (
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/welcome" component={Welcome} />
+          <Route exact path="/fridge" component={Fridge} />
+          <Route exact path="/browse" component={Browse} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/enterrecipe" component={EnterRecipe} />
+          <Route
+            exact
+            path="/searchbyingredient"
+            component={SearchByIngredient}
+          />
+          <Route exact path="/recipe/:id" component={RecipePage} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </React.Fragment>
+    );
+
+    return (
+      <Switch>
+        <App />
+      </Switch>
+    );
   }
 }
 
