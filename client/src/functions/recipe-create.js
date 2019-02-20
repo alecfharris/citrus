ebdad8a1e07c2ccbba8d204e3bdafe2a6d02c9f3
Alec Harris <alecfharris@gmail.com>
2019-02-20 14:24:49 -0600

    testing netlify deploy

--- client/src/functions/recipe-create.js
+++ client/src/functions/recipe-create.js
@@ -17,55 +17,59 @@ exports.handler = async (event, context) => {
       // disconnected from MongoDB and send them when it reconnects.
       // With serverless, better to fail fast if not connected.
       bufferCommands: false, // Disable mongoose buffering
-      bufferMaxEntries: 0 // and MongoDB driver buffering
-  });
+      bufferMaxEntries: 0, // and MongoDB driver buffering
+    });
 
-  const Recipe = conn.model('recipe', new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectID,
-    title: { type: String, required: true },
-    ingredients: { type: Array, required: true },
-    instructions: String,
-    accountId: { type: String, required: true },
-  }));
+    const Recipe = conn.model(
+      'recipe',
+      new mongoose.Schema({
+        _id: mongoose.Schema.Types.ObjectID,
+        title: { type: String, required: true },
+        ingredients: { type: Array, required: true },
+        instructions: String,
+        accountId: { type: String, required: true },
+      })
+    );
 
-  try {
-    const data = JSON.parse(event.body);
+    try {
+      const data = JSON.parse(event.body);
 
-    const { title } = data.title;
+      const { title } = data.title;
 
-    const { ingredients } = data.ingredients;
+      const { ingredients } = data.ingredients;
 
-    const { instructions } = data.instructions;
+      const { instructions } = data.instructions;
 
-    const { accountId } = data.accountId;
+      const { accountId } = data.accountId;
 
-    const id = mongoose.Types.ObjectId();
+      const id = mongoose.Types.ObjectId();
 
-    const recipe = {
-      _id: id,
-      title,
-      ingredients,
-      instructions,
-      accountId,
-    };
+      const recipe = {
+        _id: id,
+        title,
+        ingredients,
+        instructions,
+        accountId,
+      };
 
-    const response = {
-      msg: 'Recipe successfully created',
-      data: recipe,
-    };
+      const response = {
+        msg: 'Recipe successfully created',
+        data: recipe,
+      };
 
-    //   Use Recipe.Model to create a new recipe
-    await Recipe.create(recipe);
+      //   Use Recipe.Model to create a new recipe
+      await Recipe.create(recipe);
 
-    return {
-      statusCode: 201,
-      body: JSON.stringify(response),
-    };
-  } catch (err) {
-    console.log('recipe.create', err); // output to netlify function log
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: err.message }),
-    };
+      return {
+        statusCode: 201,
+        body: JSON.stringify(response),
+      };
+    } catch (err) {
+      console.log('recipe.create', err); // output to netlify function log
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ msg: err.message }),
+      };
+    }
   }
 };
