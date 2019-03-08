0aadd2c63a88efafa1f739853684fa054faf3659
Alec Harris <alecfharris@gmail.com>
2019-03-08 10:48:22 -0600

    removed commented out code

--- src/components/RecipeInput/RecipeInput.js
+++ src/components/RecipeInput/RecipeInput.js
@@ -10,55 +10,13 @@ import {
   CardContent,
   Button,
 } from './RecipeInput.styles';
-// import TextField from '@material-ui/core/TextField';
-// import Typography from '@material-ui/core/Typography';
-// import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
 
-// import StyledRecipeInput from './style.js';
 const theme = createMuiTheme({
   palette: {
     primary: { main: '#ff9966' }, // peach
     secondary: { main: '#0097A7' }, // teal
   },
 });
-
-// const styles = () => ({
-//   root: {
-//     width: '100%',
-//     // maxWidth: 360,
-//     background: '#FFF',
-//   },
-//   container: {
-//     display: 'flex',
-//     margin: 8,
-//     flexWrap: 'wrap',
-//     justifyContent: 'center',
-//   },
-//   fullTextField: {
-//     margin: 8,
-//     width: '100%',
-//   },
-//   textField: {
-//     margin: 8,
-//   },
-//   title: {
-//     fontSize: 32,
-//     display: 'flex',
-//     justifyContent: 'center',
-//   },
-//   card: {
-//     width: '90%',
-//     margin: 8,
-//     backgroundColor: 'white',
-//     padding: 8,
-//     borderRadius: 4,
-//   },
-//   button: {
-//     justifyContent: 'center',
-//     display: 'flex',
-//   },
-// });
 
 // #ff9966
 class RecipeInput extends React.Component {
